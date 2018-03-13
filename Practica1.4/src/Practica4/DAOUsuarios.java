ff15a9257b3d051c219d3d38d6913ef5ae5d940a
antonio <antonio@10.82.198.227>
2018-03-13 17:57:54 +0100

    Creación de DTO y DAO.

--- Practica1.4/src/Practica4/DAOUsuarios.java
+++ Practica1.4/src/Practica4/DAOUsuarios.java
@@ -1,5 +1,61 @@
 package Practica4;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class DAOUsuarios {
 //Conexión con base de datos
+	
+		private String DRIVER_MYSQL = "com.mysql.jdbc.Driver";
+		private String URL_MYSQL = "jdbc:mysql://localhost/usuario";
+		private Connection conn;
+		public DAOUsuarios(){
+		loadDriver();
+		}
+		private void loadDriver(){
+		try {
+		Class.forName(DRIVER_MYSQL);
+		} catch (ClassNotFoundException e) { e.printStackTrace();} }
+		private void getConnect(){
+		try {
+		conn = DriverManager.getConnection(URL_MYSQL,"root","");
+		} catch (SQLException e) { e.printStackTrace();}
+		}
+		
+		public ArrayList<DTOUsuarios> leeUsuarios(){
+			getConnect();
+			String sql = "SELECT * FROM usuarios";
+			String nombre, password, email, dni;
+			ArrayList<DTOUsuarios> listout = new ArrayList<DTOUsuarios>();
+			Statement stm = null;
+			ResultSet rs = null;
+			try{
+			stm = conn.createStatement();
+			rs = stm.executeQuery(sql);
+			while (rs.next()){
+			nombre=rs.getString("Nombre");
+			password = rs.getString("Password");
+			email=rs.getString("Email");
+			dni=rs.getString("DNI");
+			DTOUsuarios usuario = new
+			DTOUsuarios(nombre, password, email, dni);
+			listout.add(usuario);
+			}
+			} catch(SQLException e){System.out.println(e);}
+			finally{
+				if (stm!=null) {
+					try{ stm.close(); } catch(SQLException e){e.printStackTrace();}
+					}
+					if (conn!=null) {
+					try{ conn.close(); } catch(SQLException e){e.printStackTrace(); }
+					}
+					}
+			return listout;
+
 }
+}
+
--- Practica1.4/src/Practica4/DTOUsuarios.java
+++ Practica1.4/src/Practica4/DTOUsuarios.java
@@ -1,7 +1,61 @@
 package Practica4;
 
+import java.io.Serializable;
 
-public class DTOUsuarios {
+public class DTOUsuarios implements Serializable {
 //Datos de nuestra tabla
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String dni;
+
+	public DTOUsuarios() {
+		this.nombre="";
+		this.email="";
+		this.dni="";
+		this.apellido="";
+	}
+	
+	public DTOUsuarios(String nombre, String apellido, String email, String dni) {
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.email=email;
+		this.dni=dni;
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	
 	
 }
